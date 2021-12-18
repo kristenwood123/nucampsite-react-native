81203a20f549ae11e68013ebb8d41c80578cedf9
Kristen <kristenwood123@gmail.com>
2021-12-18 15:12:38 -0800

    added comments

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments'
 
 function RenderCampsite({campsite}) {
   if(campsite){
     return (
-      <Card>
-      <Card.Title>{item.name}</Card.Title>
-        {/* <Card.Image source={require('./images/react-lake.jpg')}></Card.Image> */}
+      <Card
+        featuredTitle={campsite.name}
+        image={require('./images/react-lake.jpg')}>
         <Text style={{margin: 10}}>
           {campsite.description}
         </Text>
@@ -18,12 +19,35 @@ function RenderCampsite({campsite}) {
   return <View/>
 }
 
+function RenderComments({comments}) {
+
+  const renderCommentItem = ({item}) => {
+    return (
+      <View style={{margin: 10}}>
+        <Text style={{fontSize: 14}}>{item.text}</Text>
+        <Text style={{fontSize: 12}}>{item.rating}</Text>
+        <Text style={{fontSize: 12}}>{`${item.author}, ${item.date}`}</Text>
+      </View>
+    )
+  }
+  return (
+    <Card title='Comments'>
+      <FlatList
+        data={comments}
+        renderItem={renderCommentItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </Card>
+  )
+}
+
 class CampsiteInfoComponent extends Component {
 
   constructor(props) {
     super(props)
       this.state = {
-        campsites: CAMPSITES
+        campsites: CAMPSITES,
+        comments: COMMENTS
       }
   }
 
@@ -34,7 +58,14 @@ class CampsiteInfoComponent extends Component {
   render() {
     const campsiteId = this.props.navigation.getParam('campsiteId')
     const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0]
-    return <RenderCampsite campsite={campsite} />
+    const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId)
+
+    return (
+      <ScrollView>
+        <RenderCampsite campsite={campsite} />
+        <RenderComments comments={comments} />
+      </ScrollView>
+      )
   }  
 }
 
