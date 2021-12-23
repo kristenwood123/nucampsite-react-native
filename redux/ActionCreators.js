d775c41e00b5cdad7b068445616e17a47fdd4396
Kristen <kristenwood123@gmail.com>
2021-12-23 13:25:35 -0800

    comment action types

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,22 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const postComment = (campsiteId, rating, author, text) => async dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+    newComment.date = new Date().toISOString()
+
+    setTimeout(() => {
+        dispatch(addComment(newComment))
+    }, 2000)
+}
+
+export const addComment = comment => ({
+    type: ActionType.ADD_COMMENT,
+    payload: comment
+})
