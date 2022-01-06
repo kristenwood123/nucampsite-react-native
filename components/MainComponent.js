c868c6d16d2b300e45d40fe2bf8df4ffa703bcfe
Kristen <kristenwood123@gmail.com>
2022-01-06 11:18:14 -0800

    week 4 submission

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -346,23 +346,27 @@ class Main extends Component {
     this.props.fetchPromotions();
     this.props.fetchPartners();
 
-    NetInfo.fetch().then((connectionInfo) => {
+    this.showNetInfo();
+  }
+  componentWillUnmount() {
+    this.unsubscribeNetInfo();
+  }
+
+  showNetInfo = async () => {
+    const connectionInfo = await NetInfo.fetch();
+    if (connectionInfo) {
       Platform.OS === "ios"
         ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
         : ToastAndroid.show(
             "Initial Network Connectivity Type: " + connectionInfo.type,
             ToastAndroid.LONG
           );
-    });
+    }
 
     this.unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
       this.handleConnectivityChange(connectionInfo);
     });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeNetInfo();
-  }
+  };
 
   handleConnectivityChange = (connectionInfo) => {
     let connectionMsg = "You are now connected to an active network.";
